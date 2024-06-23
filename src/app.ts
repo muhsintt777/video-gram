11c2940b1d3d134bc3571eaa1c200504ae990969
Muhsin <muhsintt777@gmail.com>
2024-06-23 17:14:40 +0530

    feat : add common middleware configs

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,11 @@ import cors from "cors";
 
 const app = express();
 
+//common middlewares
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 app.get("/", (_req, res) => {
   res.send("Hello World!");
