04bc3eba676f5a070deaa6cfbb2d0c153f6310a9
Muhsin <muhsintt777@gmail.com>
2024-06-23 18:21:06 +0530

    feat : add mongo db connection

---
+++ src/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const MONGO_URI = process.env.MONGO_URI;
+
+export const connectDB = async () => {
+  try {
+    if (!MONGO_URI) throw new Error("no db url found");
+
+    const connectionInstance = await mongoose.connect(MONGO_URI);
+    console.log(
+      `mongoDB connected!! HOST: ${connectionInstance.connection.host}`,
+    );
+  } catch (err) {
+    console.log("mongodb connection failed", err);
+    process.exit(1);
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import "dotenv/config";
 import { app } from "app";
+import { connectDB } from "db";
 const PORT = process.env.PORT || 8001;
 
-app.listen(PORT, () => {
-  return console.log(`Server is running on port: ${PORT}`);
-});
+async function startServer() {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      return console.log(`Server is running on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startServer();
