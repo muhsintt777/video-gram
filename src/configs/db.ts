5dc862ddbb4d6f951334b2d1af189a9114bf2524
Muhsin <muhsintt777@gmail.com>
2024-07-04 23:48:06 +0530

    feat : add db connection params

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -5,7 +5,10 @@ export const connectDB = async () => {
   try {
     if (!MONGO_URI) throw new Error("no db url found");
 
-    const connectionInstance = await mongoose.connect(MONGO_URI);
+    const connectionInstance = await mongoose.connect(MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+      // socketTimeoutMS: 45000,
+    });
     console.log(
       `mongoDB connected HOST: ${connectionInstance.connection.host}`,
     );
