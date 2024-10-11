eb709aa95172588e556eb58f42be9be63646d169
Muhsin <muhsintt777@gmail.com>
2024-10-11 23:56:16 +0530

    chore : install cookie parser

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { userRouter } from "modules/user/user-route";
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 app.get("/", (_req, res) => {
   res.send("Hello World!");
